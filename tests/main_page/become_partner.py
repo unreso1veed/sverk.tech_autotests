0d410feabe2aaa755e8c00e3d35caf8b909f772e
Nikita Alekseevich <puf4tank@mail.ru>
2026-03-28 23:50:36 +0700

    added two tests for main page and was maden structure of auto-tests git push git push clear kill clear

---
+++ tests/main_page/become_partner.py
@@ -0,0 +1,27 @@
+import requests
+import selenium 
+import time
+from selenium import webdriver
+from selenium.webdriver.common.keys import Keys
+from selenium.webdriver.common.by import By 
+from webdriver_manager.chrome import ChromeDriverManager
+from selenium.webdriver.chrome.service import Service
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
+
+def test_order_drone():
+    driver = webdriver.Chrome(service=Service(ChromeDriverManager().install()))
+
+    try:
+        driver.get("https://sverk.tech")
+        driver.maximize_window()
+
+        driver.find_element(By.XPATH, '//*[@id="root"]/div/main/div/section/div/div[1]/div/a[2]').click()
+        
+        wait = WebDriverWait(driver, 20)
+        wait.until(EC.visibility_of_element_located((By.XPATH, "//*[@id=\"root\"]/div/div/h1")))
+        
+        print("\033[92mMain page - become partner - passed!\033[0m")
+
+    finally:
+        driver.quit()
